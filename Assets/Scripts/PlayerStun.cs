ff563d27ddfc737ee41b2ba5cf12de4a2f8e7590
CharlesSilbergleith <csilbergleith89567@uat.edu>
2026-01-30 19:39:41 -0700

    First Project Open
    made the health and death abstract classes

---
+++ Assets/Scripts/PlayerStun.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerStun : Death
+{
+    public override void Die()
+    {
+        //TODO Stun;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+}
